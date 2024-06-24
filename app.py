c3570c359794ac914b523584f6402e9cb807bccc
hung1321 <98698290+hung1321@users.noreply.github.com>
2024-06-24 18:14:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,6 @@ app = Flask(__name__)
 @app.route("/hello")
 def home():
     return "Home"
-@app.route("/get-user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id":user_id,
-        "name":"John Doe",
-        "email":"john@gmail.com",
-    }
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
-        return jsonify(user_data),200
 @app.route("/",methods = ["POST"])
 def create_user():
     login(token="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")
